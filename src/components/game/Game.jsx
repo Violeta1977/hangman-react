efdeee372b0f06255601a9d8ec61af1cc884f65c
VioletaS <violistancikiene@gmail.com>
2024-08-16 11:52:15 +0300

    Buttons

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -2,6 +2,26 @@ import Hangman from '../img/hangman.png';
 import './Game.css'
 
 export function Game(){
+const btn = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+function handleLetterClick(){
+console.log(`Click ${btn}`);
+console.log(btn);
+
+
+}
+
+
+function renderBtn(){
+    return btn.map((letter,index) => 
+    (
+        <button className='gameBtn' key={index} onClick={() =>handleLetterClick(letter)}>{letter}</button>
+    ))
+}
+
+
+
+
     return (
         <>
   <div className='resultContainer'>
@@ -9,13 +29,11 @@ export function Game(){
     <p>POINTS</p>
   </div>
   <div className='gameContainer'>
-    
       <img src={Hangman} alt="hangman"/>
-   
     <div className='gameKeyboardContainer'>
       <div className='gameWord'>WORD</div>
       <div className='gameKeyboard'>
-        <button className='gameBtn'>A</button>
+         {renderBtn()}
       </div>
       <div className='errorsContainer'>
         <p className='errors'>0/6</p>
