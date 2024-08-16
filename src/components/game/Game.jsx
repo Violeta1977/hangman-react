fdcd981646feaf4e74c55f84fec77f2b4fb24c80
VioletaS <violistancikiene@gmail.com>
2024-08-16 10:56:11 +0300

    Style

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -1,25 +1,27 @@
 import Hangman from '../img/hangman.png';
+import './Game.css'
 
 export function Game(){
     return (
         <>
-    <div>
+  <div className='resultContainer'>
     <p>POINTS</p>
     <p>POINTS</p>
   </div>
-  <div>
-    <div>
+  <div className='gameContainer'>
+    
       <img src={Hangman} alt="hangman"/>
-    </div>
-    <div>
-      <div>WORD</div>
-      <div>
-        <button>ABC</button>
+   
+    <div className='gameKeyboardContainer'>
+      <div className='gameWord'>WORD</div>
+      <div className='gameKeyboard'>
+        <button className='gameBtn'>A</button>
       </div>
-      <div>
-        <p>YES</p>
-        <p>NO</p>
-        <button>AGAIN</button>
+      <div className='errorsContainer'>
+        <p className='errors'>0/6</p>
+        <p className='errorsText'>Nope... It was WORD </p>
+        <p className='errorsText'>Yes!!! You are so smart 🤩</p>
+        <button className='errorsBtn'>Let`s play again</button>
       </div>
     </div>
   </div>
