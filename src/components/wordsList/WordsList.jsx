9b5d7891197762d254815eec45e9725126ec5593
VioletaS <violistancikiene@gmail.com>
2024-08-19 19:40:43 +0300

    Up

--- src/components/wordsList/WordsList.jsx
+++ src/components/wordsList/WordsList.jsx
@@ -1,6 +1,11 @@
 export function WordList (){
 
-const words = ['BEAR', 'HIPPO', 'DEER', 'MONKEY', 'DOG','ALLIGATOR', 'ELEPHANT', 'JAGUAR', 'LION', 'WOLF'];
+const words = ['BEAR', 'HIPPO', 
+    'DEER', 'MONKEY', 'DOG','ALLIGATOR', 
+    'ELEPHANT', 'JAGUAR', 'LION', 'WOLF',
+    'FOX', 'GIRAFFE', 'KANGAROO', 'OTTER',
+    'PENGUIN', 'RABBIT', 'SHARK', 'TIGER', 
+    'YAK', 'ZEBRA'];
 
  return words[Math.floor(Math.random() * words.length)];
 }
