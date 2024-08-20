ebad61fe8ac3fc7e2e23372a4f83354ca62915b3
VioletaS <violistancikiene@gmail.com>
2024-08-20 09:53:13 +0300

    Random word

--- src/components/wordsList/WordsList.jsx
+++ src/components/wordsList/WordsList.jsx
@@ -5,6 +5,7 @@ const words = ['BEAR', 'HIPPO', 'FOX', 'GIRAFFE',
     'PENGUIN', 'RABBIT', 'SHARK', 'TIGER',
     'DEER', 'MONKEY', 'DOG','ALLIGATOR', 
     'ELEPHANT', 'JAGUAR', 'LION', 'WOLF'];
+const randomWord = Math.floor(Math.random() * words.length);
 
- return words[Math.floor(Math.random() * words.length)];
+return words[randomWord];
 }
